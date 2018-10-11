530de4de1577d9446150cda0f3d7797560d889c3
haodonggua98 <haodonggua98@126.com>
2018-10-11 20:43:21 +0800

    this is git on local

---
+++ src/cn/bw/hl/BaWei.java
@@ -0,0 +1,30 @@
+package cn.bw.hl;
+
+public class BaWei {
+	
+	private String baWeiChairMan;
+	private String baWeiManager;
+	
+	public BaWei(String baWeiChairMan, String baWeiManager) {
+		super();
+		this.baWeiChairMan = baWeiChairMan;
+		this.baWeiManager = baWeiManager;
+	}
+
+	public String getBaWeiChairMan() {
+		return baWeiChairMan;
+	}
+
+	public void setBaWeiChairMan(String baWeiChairMan) {
+		this.baWeiChairMan = baWeiChairMan;
+	}
+
+	public String getBaWeiManager() {
+		return baWeiManager;
+	}
+
+	public void setBaWeiManager(String baWeiManager) {
+		this.baWeiManager = baWeiManager;
+	}
+
+}
